5c21cdd26350d0fb7606518bd15869459c82a993
Ken Leland III <kenleezle@gmail.com>
2014-12-14 21:01:18 -0500

    finished lab7

--- Lab7.java
+++ Lab7.java
@@ -2,53 +2,65 @@ import java.lang.Character;
 import java.util.Scanner;
 class Lab7
 {
+  // This method only takes letters
+  // don't be dumb and send it special characters or numbers
+  // bad idea
+  public static char getNumber(char character)
+  {
+    char ch = Character.toUpperCase(character);
+    if((ch == 'A') || (ch == 'B') || (ch == 'C'))
+      {
+        ch = '2';	
+      }		
+      else if((ch =='D') || (ch =='E') || (ch =='F'))
+      {
+        ch = '3';
+      }
+      else if((ch =='G') || (ch =='H') || (ch =='I'))
+      {
+        ch = '4';
+      }
+      else if((ch =='J') || (ch =='K') || (ch =='L'))
+      {
+        ch = '5';
+      }
+      else if((ch =='M') || (ch =='N') || (ch =='O'))
+      {
+        ch = '6';
+      }
+      else if((ch =='P') || (ch =='Q') || (ch =='R') || (ch =='S'))
+      {	
+        ch = '7';	
+      }
+      else if((ch =='T') || (ch =='U') || (ch =='V'))
+      {
+        ch = '8';
+      }
+      else if((ch =='W') || (ch =='X') || (ch =='Y') || (ch =='Z'))
+      {
+        ch = '9';
+      }
+      return ch;
+  }
 	public static void main(String[] args) 
 	{
 		int x;
-		String a;
+
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter a 1800 number");
-		a= in.nextLine();
-		//a.toLowerCase();
-		b.toLowerCase(a);
-		//char ch = a.CharAt(x);
-		for(x=0;x<a.length;++x)
-		{
-			char ch = b[x];
-			if((ch == 'a') || (ch == 'b') || (ch == 'c'))
-				{
-					ch = 2;	
-				}		
-				else if((ch =='d') || (ch =='e') || (ch =='f'))
-				{
-					ch = 3;
-				}
-				else if((ch =='g') || (ch =='h') || (ch =='i'))
-				{
-					ch = 4;
-				}
-				else if((ch =='j') || (ch =='k') || (ch =='l'))
-				{
-					ch = 5;
-				}
-				else if((ch =='m') || (ch =='n') || (ch =='o'))
-				{
-					ch = 6;
-				}
-				else if((ch =='p') || (ch =='q') || (ch =='r') || (ch =='s'))
-				{	
-					ch = 7;	
-				}
-				else if((ch =='t') || (ch =='u') || (ch =='v'))
-				{
-					ch = 8;
-				}
-				else if((ch =='w') || (ch =='x') || (ch =='y') || (ch =='z'))
-				{
-					ch = 9;
-				}
-		}
-		System.out.println(ch);
+		System.out.println("Enter a string: ");
+		String a = in.nextLine();
+    String translated_string = "";
+
+		for(x=0;x<a.length();++x)
+    {
+      char character = a.charAt(x);
+
+      if(Character.isLetter(character))
+        character = getNumber(character);
+
+      translated_string = translated_string + character;
+    }
+    System.out.println(translated_string);
 	}
 }
 			
