82773d2f678b3ca40586702fdb8fd181ef306e82
jjordan9 <jeffthemag@gmail.com>
2014-12-15 19:58:24 -0500

    Lab12

--- Lab12.java
+++ Lab12.java
@@ -2,14 +2,16 @@ class Lab12
 {
 	public static void main(String[] args) 
 	{
-		double w, newbalance;
+		double w, balance,a;
 		Account acc1 = new Account();
 		double id = acc1.getId();
-		double balance = acc1.getBal();
+		balance = acc1.getBal();
 		double annualInterestRate = acc1.getRate();
-		newbalance = acc1.withdraw(w);
-		System.out.println(newbalance);
-
+		balance = acc1.withdraw(balance);
+		balance = acc1.deposit();
+		System.out.println(balance);
+		
+		
 		
 
 
--- Lab7.java
+++ Lab7.java
@@ -2,9 +2,6 @@ import java.lang.Character;
 import java.util.Scanner;
 class Lab7
 {
-  // This method only takes letters
-  // don't be dumb and send it special characters or numbers
-  // bad idea
   public static char getNumber(char character)
   {
     char ch = Character.toUpperCase(character);
@@ -45,22 +42,23 @@ class Lab7
 	public static void main(String[] args) 
 	{
 		int x;
-
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter a string: ");
+		System.out.print("Enter a string: ");
 		String a = in.nextLine();
-    String translated_string = "";
-
+		String translated_string = "";
 		for(x=0;x<a.length();++x)
-    {
-      char character = a.charAt(x);
-
-      if(Character.isLetter(character))
-        character = getNumber(character);
-
-      translated_string = translated_string + character;
-    }
-    System.out.println(translated_string);
+		{
+			char character = a.charAt(x);
+			if(Character.isLetter(character))
+			{
+				character = getNumber(character);
+			}
+			translated_string = translated_string + character;
+		}
+		System.out.println(translated_string);
 	}
 }
-			
+/*==============================================================
+Enter a string: 1-800-Flowers
+1-800-3569377
+
